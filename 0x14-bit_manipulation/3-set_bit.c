f2aac0d6e5c2c6afd06468bdc63c270d23d06150
Reechy254 <richardochola3@gmail.com>
2023-09-29 19:30:06 +0300

    do to the end

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	i = 1 << index;
+	b = 1 << index;
 	*n = *n | b;
 
 	return (1);
